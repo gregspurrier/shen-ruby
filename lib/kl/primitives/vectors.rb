fd0d261b22ab0a60964bec7ab32928cfdedc867e
Greg Spurrier <greg@sourcematters.org>
2012-12-22 07:33:37 -0800

    Optimize vector creation
    Also add paranoid checks for debugging

--- lib/kl/primitives/vectors.rb
+++ lib/kl/primitives/vectors.rb
@@ -2,15 +2,25 @@ module Kl
   module Primitives
     module Vectors
       def absvector(n)
-        Kl::Absvector.new
+        Kl::Absvector.new(n)
       end
 
       define_method "address->" do |v, n, value|
+        raise Kl::InternalError, 'nil value' if value.nil?
+
+        if n < 0 || n >= v.upper_limit
+          raise Kl::Error, "out of bounds"
+        end
+
         v[n] = value
         v
       end
 
       define_method "<-address" do |v, n|
+        if n < 0 || n >= v.upper_limit
+          raise Kl::Error, "out of bounds"
+        end
+
         v[n]
       end
 
