e773dd389930e80a06eb0c8512b830ab8b99c2d4
Greg Spurrier <greg@sourcematters.org>
2012-12-26 13:44:11 -0800

    Add script for running test suite

---
+++ bin/shen_test_suite.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+require 'shen_ruby'
+
+env = ShenRuby::Environment.new
+env.__eval(Kl::Cons.list([:cd, "shen_src/test_programs"]))
+env.__eval(Kl::Cons.list([:load, "README.shen"]))
+env.__eval(Kl::Cons.list([:load, "tests.shen"]))
