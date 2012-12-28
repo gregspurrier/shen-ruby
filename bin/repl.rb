8a5ae053659547295166a58a7221e6c4a35f29ec
Greg Spurrier <greg@sourcematters.org>
2012-12-28 08:23:10 -0800

    Better handling of Ruby exceptions

--- bin/repl.rb
+++ bin/repl.rb
@@ -3,9 +3,13 @@ $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'shen_ruby'
 
 env = ShenRuby::Environment.new
+command = :"shen-shen"
 begin
-  env.__eval(Kl::Cons.list([:"shen-shen"]))
-rescue Kl::Error => e
-  puts e.message
+  env.__eval(Kl::Cons.list([command]))
+rescue StandardError => e
+  # K Lambda simple error
+  puts "Ruby exception: #{e.message}"
+  # Return to the Shen interpreter without re-printing the credits
+  command = :"shen-loop"
   retry
 end
