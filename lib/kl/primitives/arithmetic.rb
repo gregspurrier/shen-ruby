3e61a22a112048ece976e5ababe47b2e9bc7645f
Greg Spurrier <greg@sourcematters.org>
2012-12-29 10:08:38 -0800

    Raise simple-errors when arguments are not numeric
    This fixes the case of evaluating '(@p 1 2)' in the REPL.

--- lib/kl/primitives/arithmetic.rb
+++ lib/kl/primitives/arithmetic.rb
@@ -2,18 +2,26 @@ module Kl
   module Primitives
     module Arithmetic
       def +(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a + b
       end
       
       def -(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a - b
       end
       
       def *(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a * b
       end
       
       def /(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         if a.kind_of?(Fixnum) && b.kind_of?(Fixnum) && a % b != 0
           a = a.to_f
         end
@@ -21,18 +29,26 @@ module Kl
       end
       
       def >(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a > b
       end
 
       def <(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a < b
       end
       
       def >=(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a >= b
       end
       
       def <=(a, b)
+        raise ::Kl::Error, "#{a} is not a number" unless a.kind_of? Numeric
+        raise ::Kl::Error, "#{b} is not a number" unless b.kind_of? Numeric
         a <= b
       end
       
