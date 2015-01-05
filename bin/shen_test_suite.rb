41f249719a1f83e79a50ebd358e1361b449f23dc
Greg Spurrier <greg@sourcematters.org>
2015-01-04 19:42:57 -0800

    Prepare for running test suite under Travis CI
    - Override y-or-n? so that the script does not pause for input on
      failure.
    - Exit with a non-zero status if any failures are encountered

--- bin/shen_test_suite.rb
+++ bin/shen_test_suite.rb
@@ -6,4 +6,16 @@ require 'shen_ruby'
 shen = ShenRuby::Shen.new
 shen.cd('shen/release/test_programs')
 shen.load('README.shen')
+
+# Override y-on-n? so that the script continues on error.
+# We must use KLambda's defun to circumvent Shen's protection
+# against redefining system functions.
+shen.eval_string('(defun y-or-n? (Ignored) true)')
+
+# Reset the pass/fail counters now and then make reset a no-op
+# so that we can query it at the end of the test run.
+shen.reset
+shen.eval_string('(define reset -> true)')
+
 shen.load('tests.shen')
+exit(1) unless shen.value(:"test-harness.*failed*") == 0
